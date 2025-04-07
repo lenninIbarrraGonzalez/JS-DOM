90b41750c59edc6ae37d1832dfb961c6fc18c186
Lennin Ibarra <ing.lenninibarra@gmail.com>
2025-04-07 11:27:42 -0500

    se modifica el countdown y se agrega el scroll up

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import hamburgerMenu from "./js/menu_hamburguer.js";
 import {digitalOclock, alarm} from "./js/timer_alarm.js";
 import {move_object, short_Cuts} from "./js/move_object.js";
-import countdown from "./cuenta_regresiva.js";
+import countdown from "./js/cuenta_regresiva.js"
+import scroll_up from "./js/scroll_up.js";
 
 const d = document;
 d.addEventListener("DOMContentLoaded", () => {
@@ -17,5 +18,6 @@ d.addEventListener("DOMContentLoaded", () => {
     move_object(".circle");
     short_Cuts();
     countdown("count-down","Apr 04,2030 11:23:19", "LLego la hora 🤓");
+    scroll_up(".btn-up")
 }); 
 
---
+++ js/scroll_up.js
@@ -0,0 +1,25 @@
+export default function scroll_up(btnup) {
+    const $btn_up = document.querySelector(btnup);
+    const $btn_up_classlist = $btn_up.classList;
+    //console.log($btn_up_classlist);
+    
+
+    document.addEventListener("scroll", () => {
+        //console.log(window.scrollY);
+        if (window.scrollY  > 100) {
+            $btn_up_classlist.remove("btn-inactive");
+        }else {
+            $btn_up_classlist.add("btn-inactive");
+        }
+    });
+
+    document.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth" // para que suba suavemente
+        });
+    });
+
+    
+
+}
